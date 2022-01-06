7d26bd8e3fffb8dd3b49b93c61922b5706d5e7bf
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-01-06 16:45:33 +0100

    forth commit'Update, Delete'

--- main.go
+++ main.go
@@ -8,16 +8,12 @@ import (
 
 func main() {
 	dictionary := mydict.Dictionary{}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	baseWord := "hello"
+	dictionary.Add(baseWord, "first")
+	err := dictionary.Update(baseWord, "Second")
 	if err != nil {
 		fmt.Println(err)
 	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println("found", word, "defination:", hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
 }
